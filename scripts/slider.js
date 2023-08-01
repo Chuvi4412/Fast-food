42d16b79f4770f6d6206b6d4d178be9c5c230a60
Chuvi4412 <jdirzo@hotmail.com>
2023-07-31 21:11:26 -0600

    finalizacion de slideru creacion de tarjetas de menu

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
     $('.cards-slider').slick({
-        
+        autoplay: true,
+        infinite: true,
+        autoplaySpeed: 3000,
+        pauseOnHover:false,
     });
   });
